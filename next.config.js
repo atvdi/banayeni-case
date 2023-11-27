bd75ea7ee88f28c0fdb17dcff74914581a8c0a9d
Abdullah Tanrıverdi <89466000+atvdi@users.noreply.github.com>
2023-11-27 11:58:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: { remotePatterns: [{ protocol: "https", hostname: "i0.shbdn.com" }] },
 };
